4055b6879a4db946f1e3699f0271b17916cea121
adilamin-dev <adilamin537@gmail.com>
2022-12-21 07:43:37 -0500

    Wining score done.

--- dist/js/app.js
+++ dist/js/app.js
@@ -9,9 +9,10 @@ var botScore = 0;
 
 
 
-rockBtn.addEventListener('click', rock)
-paperBtn.addEventListener('click', paper)
-scissorsBtn.addEventListener('click', scissors)
+rockBtn.addEventListener('click', rock);
+paperBtn.addEventListener('click', paper);
+scissorsBtn.addEventListener('click', scissors);
+
 
 function rock() {
     let hpHand = document.querySelector('.human-player-hands>img');
@@ -23,14 +24,43 @@ function rock() {
     if(botTurn === 0){
         bpHand.setAttribute('src', './dist/img/b-rock.svg');
         
+        if(yourScore === 50 && yourScore > botScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: green;">You WIN!</span> 🥳'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
+        
+        if(botScore === 50 && botScore > yourScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: red;">You LOSE!</span> 🥲'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
     } else if(botTurn === 1){
         bpHand.setAttribute('src', './dist/img/b-paper.svg');
         botScore += 1;
         document.querySelector('.b-score').textContent = botScore;
+        
+        if(yourScore === 50 && yourScore > botScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: green;">You WIN!</span> 🥳'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
+        
+        if(botScore === 50 && botScore > yourScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: red;">You LOSE!</span> 🥲'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
     } else {
         bpHand.setAttribute('src', './dist/img/b-scissors.svg');
         yourScore += 1;
         document.querySelector('.h-score').textContent = yourScore;
+
+        if(yourScore === 50 && yourScore > botScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: green;">You WIN!</span> 🥳'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
+        
+        if(botScore === 50 && botScore > yourScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: red;">You LOSE!</span> 🥲'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
     }
 }
 
@@ -43,12 +73,42 @@ function paper() {
         bpHand.setAttribute('src', './dist/img/b-rock.svg');
         yourScore += 1;
         document.querySelector('.h-score').textContent = yourScore;
+
+        if(yourScore === 50 && yourScore > botScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: green;">You WIN!</span> 🥳'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
+        
+        if(botScore === 50 && botScore > yourScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: red;">You LOSE!</span> 🥲'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
     } else if(botTurn === 1){
         bpHand.setAttribute('src', './dist/img/b-paper.svg');
+
+        if(yourScore === 50 && yourScore > botScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: green;">You WIN!</span> 🥳'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
+        
+        if(botScore === 50 && botScore > yourScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: red;">You LOSE!</span> 🥲'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
     } else {
         bpHand.setAttribute('src', './dist/img/b-scissors.svg');
         botScore += 1;
         document.querySelector('.b-score').textContent = botScore;
+
+        if(yourScore === 50 && yourScore > botScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: green;">You WIN!</span> 🥳'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
+        
+        if(botScore === 50 && botScore > yourScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: red;">You LOSE!</span> 🥲'
+            document.getElementById('pop').classList.remove("popup-rm");
+        }
     }
 
 }
@@ -62,11 +122,61 @@ function scissors() {
         bpHand.setAttribute('src', './dist/img/b-rock.svg');
         botScore += 1;
         document.querySelector('.b-score').textContent = botScore;
+
+        if(yourScore === 50 && yourScore > botScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: green;">You WIN!</span> 🥳'
+            document.getElementById('.pop').classList.remove("popup-rm");
+        }
+        
+        if(botScore === 50 && botScore > yourScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: red;">You LOSE!</span> 🥲'
+            document.getElementById('.pop').classList.remove("popup-rm");
+        }
+        
     } else if(botTurn === 1){
         bpHand.setAttribute('src', './dist/img/b-paper.svg');
         yourScore += 1;
         document.querySelector('.h-score').textContent = yourScore;
+
+        if(yourScore === 50 && yourScore > botScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: green;">You WIN!</span> 🥳'
+            document.getElementById('.pop').classList.remove("popup-rm");
+        }
+        
+        if(botScore === 50 && botScore > yourScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: red;">You LOSE!</span> 🥲'
+            document.getElementById('.pop').classList.remove("popup-rm");
+        }
+        
     } else {
         bpHand.setAttribute('src', './dist/img/b-scissors.svg');
+
+        if(yourScore === 50 && yourScore > botScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: green;">You WIN!</span> 🥳'
+            document.getElementById('.pop').classList.remove("popup-rm");
+        }
+        
+        if(botScore === 50 && botScore > yourScore){
+            document.querySelector('.pModals>h2').innerHTML = '<span style="color: red;">You LOSE!</span> 🥲'
+            document.getElementById('.pop').classList.remove("popup-rm");
+        }
+        
     }
+}
+
+
+
+
+
+
+let playAgainBtn = document.getElementById('play-again');
+
+playAgainBtn.addEventListener('click', playAgain);
+
+function playAgain() {
+    document.querySelector('.popup').classList.add("popup-rm")
+    yourScore *= 0;
+    botScore *= 0;
+    document.querySelector('.h-score').textContent = yourScore;
+    document.querySelector('.b-score').textContent = botScore;
 }
